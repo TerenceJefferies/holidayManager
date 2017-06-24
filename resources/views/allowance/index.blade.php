d66a37d06b34c166180fd069744f1c0c13c5f78e
Terence Jefferies <terencejefferies@hotmail.com>
2017-06-24 19:05:24 +0100

    Updated allowance index view to have a more accessible list view

--- resources/views/allowance/index.blade.php
+++ resources/views/allowance/index.blade.php
@@ -3,28 +3,28 @@
 @section('content')
   <h1>Your Allowance</h1>
   <div class="row">
-    <div class="col-md-6">Days</div>
-    <div class="col-md-6">{{ $allowance -> days }}</div>
+    <div class="col-xs-6 col-sm-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-lg-6">Days</div>
+    <div class="col-xs-6 col-sm-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-lg-6">{{ $allowance -> days }}</div>
   </div>
   <div class="row">
-    <div class="col-md-6">Allowance Start Date</div>
-    <div class="col-md-6">{{ $allowance -> starts -> format('d/m/Y') }}</div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">Allowance Start Date</div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">{{ $allowance -> starts -> format('d/m/Y') }}</div>
   </div>
   <div class="row">
-    <div class="col-md-6">Allowance End Date</div>
-    <div class="col-md-6">{{ $allowance -> ends -> format('d/m/Y') }}</div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">Allowance End Date</div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">{{ $allowance -> ends -> format('d/m/Y') }}</div>
   </div>
   <div class="row">
-    <div class="col-md-6">Days Redeemed</div>
-    <div class="col-md-6">{{ $daysUsed }}</div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">Days Redeemed</div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">{{ $daysUsed }}</div>
   </div>
   <div class="row">
-    <div class="col-md-6">Requests Accepted</div>
-    <div class="col-md-6">{{ $acceptedRequestsCount }}</div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">Requests Accepted</div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">{{ $acceptedRequestsCount }}</div>
   </div>
   <div class="row">
-    <div class="col-md-6">Requests Rejected</div>
-    <div class="col-md-6">{{ $rejectedRequestsCount }}</div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">Requests Rejected</div>
+    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">{{ $rejectedRequestsCount }}</div>
   </div>
   <!-- Upcoming Allowance -->
 @endsection('content')
