ed98eaa4908d5a2818d90191d0c35eea5cf61bd5
Terence Jefferies <terencejefferies@hotmail.com>
2017-07-01 13:02:42 +0100

    Updated HolidayExpenditureRepositoryInterface to meet the new requirements of the expenditure repository

--- app/HolidayManager/HolidayTime/HolidayExpenditureRepositoryInterface.php
+++ app/HolidayManager/HolidayTime/HolidayExpenditureRepositoryInterface.php
@@ -1,9 +1,12 @@
 <?php
 namespace App\HolidayManager\HolidayTime;
 
+use Carbon\Carbon;
+
 interface HolidayExpenditureRepositoryInterface{
   public function getByAllowanceId($id);
-  public function getExpendituresForAllowance(HolidayAllowanceInterface $holidayAllowance,Bool $showUnapproved=true,Int $limit=0);
+  public function getExpendituresForAllowance(HolidayAllowanceInterface $holidayAllowance,Bool $showUnapproved=true,Int $limit=0,$orderField='created_at',$orderDirection='desc');
   public function getNextExpenditureForAllowance(HolidayAllowanceInterface $holidayAllowance,$showUnapproved=false);
-  public function getExpendituresForAllowanceByStatus(HolidayAllowanceInterface $holidayAllowance,$status);
+  public function getExpendituresForAllowanceByStatus(HolidayAllowanceInterface $holidayAllowance,$status,Int $limit=0,$orderField='created_at',$orderDirection='desc');
+  public function createByAllowanceId($allowanceId,Carbon $startDate,$days);
 }
